b6a76b7ed4145f5f1036fec12b8c46dad57fd154
marsel23xxx <marsel23xxx@gmail.com>
2025-08-04 08:31:52 +0700

    first commit

---
+++ src/main/java/com/skripsi/SAW_Erajaya_Group.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.skripsi;
+
+/**
+ *
+ * @author User
+ */
+public class SAW_Erajaya_Group {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
